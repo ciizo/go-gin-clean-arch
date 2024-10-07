cc80a459f0370844c085ec39f5e91129d093962c
chatupon khooha <chatupon.kh@pi.financial>
2024-10-08 01:31:18 +0700

    add api server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,18 +3,16 @@ package main
 import (
 	"fmt"
 
+	"github.com/ciizo/go-gin-clean-arch/cmd/api/server"
 	"github.com/ciizo/go-gin-clean-arch/config"
-	"github.com/ciizo/go-gin-clean-arch/internal/user/handlers"
-
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	cfg := config.New().LoadConfigurations()
-	svUrl := fmt.Sprintf("%s:%d", cfg.Server.Hostname, cfg.Server.Port)
+	srvUrl := fmt.Sprintf("%s:%d", cfg.Server.Hostname, cfg.Server.Port)
 
-	router := gin.Default()
-	router.GET("/users", handlers.GetUsers)
+	srv := server.New(srvUrl)
+	srv.RegisterRoutes()
 
-	router.Run(svUrl)
+	srv.Start()
 }
--- cmd/api/server/server.go
+++ cmd/api/server/server.go
@@ -1,1 +1,29 @@
 package server
+
+import (
+	"github.com/ciizo/go-gin-clean-arch/internal/user/handlers"
+	"github.com/gin-gonic/gin"
+)
+
+type ApiServer struct {
+	url    string
+	router *gin.Engine
+}
+
+func New(url string) *ApiServer {
+	g := gin.Default()
+
+	return &ApiServer{url: url, router: g}
+}
+
+func (api *ApiServer) RegisterRoutes() {
+	registerUserHandler(api.router)
+}
+
+func (api *ApiServer) Start() {
+	api.router.Run(api.url)
+}
+
+func registerUserHandler(router *gin.Engine) {
+	router.GET("/users", handlers.GetUsers)
+}
